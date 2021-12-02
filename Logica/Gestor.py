c25cd9603f6a7b683b596aa43b33a3b0ea206056
Jesus Lozada <jeallomo@hotmail.com>
2021-12-02 01:40:27 -0500

    gestor.py

--- Logica/Gestor.py
+++ Logica/Gestor.py
@@ -25,6 +25,24 @@ class Gestor:
             proceso = self.procesos[len(self.procesos)-1]
             self.fillMemoria(proceso.getBase(), proceso.getBase() + (int)(proceso.getTamaño()/4), proceso.getPID())
             self.count += 1
+        elif self.formatoMemoria == 2:
+            particionAsignada = self.getBaseEstaticaVariable((int)(tamaño/4))
+            self.procesos.append(Process(self.count, particionAsignada[0], tamaño, nombre, 0, particionAsignada[1]))
+            proceso = self.procesos[len(self.procesos)-1]
+            self.fillMemoria(proceso.getBase(), proceso.getBase() + (int)(proceso.getTamaño()/4), proceso.getPID())
+            self.count += 1
+        elif self.formatoMemoria == 3:
+            particionAsignada = self.getBaseDinamicaSin((int)(tamaño/4))
+            self.procesos.append(Process(self.count, particionAsignada[0], tamaño, nombre, 0, particionAsignada[1]))
+            proceso = self.procesos[len(self.procesos)-1]
+            self.fillMemoria(proceso.getBase(), proceso.getBase() + (int)(proceso.getTamaño()/4), proceso.getPID())
+            self.count += 1
+        elif self.formatoMemoria == 1:
+            particionAsignada = self.getBaseDinamicaCon((int)(tamaño/4))
+            self.procesos.append(Process(self.count, particionAsignada[0], tamaño, nombre, 0, particionAsignada[1]))
+            proceso = self.procesos[len(self.procesos)-1]
+            self.fillMemoria(proceso.getBase(), proceso.getBase() + (int)(proceso.getTamaño()/4), proceso.getPID())
+            self.count += 1
 
     def getBaseEstaticaFija(self, tamaño: int) -> list:
         for part in self.particiones:
@@ -33,13 +51,13 @@ class Gestor:
                 return [(int)(part[0]), self.particiones.index(part)]
         return [-1,-1]
 
-    def getBaseEstaticaVariable(self, tamaño: int) -> int:
-        particion = 0       
+    def getBaseEstaticaVariable(self, tamaño: int) -> list:
+        particion = 0 
+        diferencia = -1      
 
         if self.ajuste == 1:
-            diferencia = self.particiones[0][1] - self.particiones[0][0]
             for x in range(1,len(self.particiones)):
-                if (self.particiones[x][1] - self.particiones[x][0]) < diferencia and self.particiones[x][2] == False:
+                if ((self.particiones[x][1] - self.particiones[x][0]) < diferencia or diferencia == -1) and self.particiones[x][2] == False and (self.particiones[x][1] - self.particiones[x][0]) > tamaño:
                     diferencia = self.particiones[x][1] - self.particiones[x][0]
                     particion = x
             if particion != 0:
@@ -47,9 +65,8 @@ class Gestor:
             else:
                 return [-1,-1]
         elif self.ajuste == 2:
-            diferencia = self.particiones[0][1] - self.particiones[0][0]
             for x in range(1,len(self.particiones)):
-                if (self.particiones[x][1] - self.particiones[x][0]) > diferencia and self.particiones[x][2] == False:
+                if ((self.particiones[x][1] - self.particiones[x][0]) > diferencia or diferencia == -1) and self.particiones[x][2] == False and (self.particiones[x][1] - self.particiones[x][0]) > tamaño:
                     diferencia = self.particiones[x][1] - self.particiones[x][0]
                     particion = x
             if particion != 0:
@@ -62,11 +79,44 @@ class Gestor:
                     return [self.particiones[x][0], x]
         return [-1,-1]
 
-    def getBaseDinamicaSin(self) -> int:
-        return 0
+    def getBaseDinamicaSin(self, tamaño: int) -> list:
+        particionLibre = False
 
-    def getBaseDinamicaCon(self) -> int:
-        return 0
+        for x in self.particiones:
+            if x[2] == False and x[1]-x[0] >= tamaño:
+                particionLibre = True
+        
+        if particionLibre == True:
+            diferencia = -1
+            particion = 0
+            if self.ajuste == 1:
+                for x in range(1,len(self.particiones)):
+                    if ((self.particiones[x][1] - self.particiones[x][0]) < diferencia or diferencia == -1) and self.particiones[x][2] == False and (self.particiones[x][1] - self.particiones[x][0]) > tamaño:
+                        diferencia = self.particiones[x][1] - self.particiones[x][0]
+                        particion = x
+            elif self.ajuste == 2:
+                for x in range(1,len(self.particiones)):
+                    if ((self.particiones[x][1] - self.particiones[x][0]) > diferencia or diferencia == -1) and self.particiones[x][2] == False and (self.particiones[x][1] - self.particiones[x][0]) > tamaño:
+                        diferencia = self.particiones[x][1] - self.particiones[x][0]
+                        particion = x
+            elif self.ajuste == 3:
+                for x in range(1,len(self.particiones)):
+                    if (self.particiones[x][1] - self.particiones[x][0]) > tamaño and self.particiones[x][2] == False:
+                        particion = x
+            return [self.particiones[particion][0], particion]
+        else:
+            if len(self.particiones == 0):
+                self.particiones.append([0, tamaño, True])
+            else:
+                self.particiones.append([self.particiones[len(self.particiones)-1]+1, self.particiones[len(self.particiones)-1]+1+tamaño, False])
+            return [self.particiones[len(self.particiones)-1][0], len(self.particiones)-1]
+
+    def getBaseDinamicaCon(self, tamaño: int) -> list:
+        if len(self.particiones == 0):
+            self.particiones.append([0, tamaño, True])
+        else:
+            self.particiones.append([self.particiones[len(self.particiones)-1]+1, self.particiones[len(self.particiones)-1]+1+tamaño, False])
+        return [self.particiones[len(self.particiones)-1][0], len(self.particiones)-1]
 
     def setMejorAjuste(self) -> None:
         self.ajuste = 1
